4333a6de0501fabbacadae42474c665dd567d0f5
Charles-Étienne <202233006@collegeahuntsic.qc.ca>
2024-08-30 12:31:47 -0400

    Initial Commit

---
+++ Projet1iOS/Projet1iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet1iOSApp.swift
+//  Projet1iOS
+//
+//  Created by Charles-Étienne Dubé (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct Projet1iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
